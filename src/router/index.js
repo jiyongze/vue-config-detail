b3de9c677a64fcf4593d3f97d4935136f8fe76f3
jyz_coder <1270692931@qq.com>
2018-08-28 23:46:40 +0800

    配置按需加载和404页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+const notFound = resolve => require.ensure([], () => resolve(require('@/pages/404')), '404')
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '*', /* 当路由匹配不到路径是渲染404页面 */
+      name: 'notFound',
+      component: notFound
     }
   ]
 })
